2b4abbde0f091a8c8ad983f740b7c81e8f772473
Iscaron/Sol <dmitry.aka.sol@yandex.ru>
2018-06-10 13:15:14 +0300

    html initial

--- structure/views.py
+++ structure/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Table
+
+# Table.objects.filter(published_date__lte=timezone.now()).order_by('change_date')
 
 # Create your views here.
+
+def tables_list(request):
+    table = Table.objects.filter(change_date__lte=timezone.now()).order_by('change_date')
+    return render(request, 'structure/tables_list.html', {'tables': table})
