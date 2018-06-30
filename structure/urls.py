a2493143fe615e65dc664d480e89ee91471de1a5
Iscaron/Sol <dmitry.aka.sol@yandex.ru>
2018-06-30 16:46:34 +0300

    add login form

--- structure/urls.py
+++ structure/urls.py
@@ -9,4 +9,5 @@ urlpatterns = [
     url(r'^table/new/$', views.table_new, name='table_new'),
     url(r'^table/(?P<pk>\d+)/edit/$', views.table_edit, name='table_edit'),
     url(r'^register/$', views.register, name='register'),
+    url(r'^login/$', views.user_login, name='login'),
 ]
